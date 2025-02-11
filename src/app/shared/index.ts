00deeeda854ab34e3e13c65fc3162afe9744d8df
RyanDrop <ryangithub7509@gmail.com>
2025-02-11 20:43:24 -0300

    ✨feat: deck card component with styling and models

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,3 +1,5 @@
+export * from './constants';
 export * from './enums';
+export * from './models';
 export * from './services';
 export * from './transloco';
---
+++ src/app/widgets/components/deck-card/deck-card.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { ICard } from '@models';
+
+@Component({
+  selector: 'pd-deck-card',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    @let card = this.card();
+    @let isJoker = card.visibleValue == 'joker';
+    
+    <div [ngClass]="['card', card.color]">
+      <div class="left"><ng-container class="left" [ngTemplateOutlet]="isJoker ? joker : commonExtremities"/></div>
+      <div class="center"><ng-container class="center" [ngTemplateOutlet]="isJoker ? joker : commonCenter"/></div>
+      <div class="right"><ng-container class="right" [ngTemplateOutlet]="isJoker ? joker : commonExtremities"/></div>
+    </div>
+
+    <ng-template #joker><img [src]="jokerImgSrc()" alt="card-joker-icon"/></ng-template>
+    <ng-template #commonCenter>{{ card.suitIcon }}</ng-template>
+    <ng-template #commonExtremities>{{ card.visibleValue }} {{ card.suitIcon }}</ng-template>
+  `,
+  styleUrls: ['./deck-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DeckCardComponent {
+  public readonly card = input.required<ICard>();
+
+  protected readonly jokerImgSrc = computed(() => {
+    const card = this.card();
+    return `./assets/icons/${card.color}-joker.png`;
+  });
+}
